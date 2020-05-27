1389fdfaf736223821d6cd9ccbf28d51807a577f
Simon Wathigo <wathigosimon@gmail.com>
2020-05-27 10:37:12 +0300

    Updates stypes for a filters global data

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -7,18 +7,18 @@ import { faArrowAltCircleLeft} from '@fortawesome/free-solid-svg-icons'
 
 function GlobalFilter(props) {
     return (
-        <div className='global'>
+        <div className='global filtered-country'>
             <FontAwesomeIcon onClick={(event) => {props.back(event)}} icon={faArrowAltCircleLeft} />
             <h1> Global Covid-19 Official Data Analytics </h1>
 
-                <div className='global-container'>                                                                     
+                <div className='country-container'>                                                                     
                     <p><b>New Confirmed:</b> {props.globalData.NewConfirmed}</p>
                     <p><b>Total Confirmed</b> {props.globalData.TotalConfirmed}</p>
                     <p><b>New Deaths</b> {props.globalData.NewDeaths}</p>
                     <p><b>New Recovered</b> {props.globalData.NewRecovered}</p>
                     <p><b>Total Deaths</b> {props.globalData.TotalDeaths}</p>
                     <p><b>Total Recovered</b> {props.globalData.TotalRecovered}</p>
-                    <span>Date: {props.date}</span>
+                    <p>Date: {props.date}</p>
                 </div>
              
 
