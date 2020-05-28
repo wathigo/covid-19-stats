59f553d9aca19c3b1d244f1b9622293682e209f2
Simon Wathigo <wathigosimon@gmail.com>
2020-05-28 10:15:03 +0300

    Fixes lint errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './App.css';
 
 import Home from './components/Home';
 import * as ActionCreators from './actions';
 
 function App(props) {
-  useEffect(() => { 
+  useEffect(() => {
     props.fetchData();
-  }, [props]);
+  }, []);
 
   return (
-      <div className="App">
-        <Home/>
-      </div>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
 const mapStateToProps = (state => {
   return state;
 });
+
+App.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, ActionCreators)(App);
