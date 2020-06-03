9c5edb6bbf5c4fb855a5187a659d15ce3d9b0947
Simon Wathigo <wathigosimon@gmail.com>
2020-06-03 09:58:36 +0300

    Fixes lint errors

--- tests/actions_test.js
+++ tests/actions_test.js
@@ -3,58 +3,54 @@ import * as ActionCreators from '../src/actions';
 describe('actions', () => {
   describe(ActionCreators.updateData, () => {
     const sampleData = {
-      "data": [
+      data: [
         {
-          'name': "Algeria",
-          'deaths': 400,
+          name: 'Algeria',
+          deaths: 400,
         },
         {
-          'name': 'Kenya',
-          'deaths': 60,
-        }
-      ]
+          name: 'Kenya',
+          deaths: 60,
+        },
+      ],
     };
-    it("It should return expected action", () => {
-
+    it('It should return expected action', () => {
       const expectedAction = {
         type: 'GET_ALL_DATA',
-        'state': sampleData
-      }
-      expect(ActionCreators.updateData(sampleData)).toEqual(expectedAction)
-    })
-  })
+        state: sampleData,
+      };
+      expect(ActionCreators.updateData(sampleData)).toEqual(expectedAction);
+    });
+  });
 
   describe(ActionCreators.loading, () => {
     it('It should return expected action', () => {
-
       const expectedAction = {
         type: 'LOADING',
-        'isLoading': false
-      }
-      expect(ActionCreators.loading()).toEqual(expectedAction)
-    })
-  })
+        isLoading: false,
+      };
+      expect(ActionCreators.loading()).toEqual(expectedAction);
+    });
+  });
 
   describe(ActionCreators.changeFilter, () => {
     const filter = 'Kenya';
     it('It should return expected action', () => {
-
       const expectedAction = {
         type: 'CHANGE_FILTER',
-        'filter': filter,
-      }
-      expect(ActionCreators.changeFilter(filter)).toEqual(expectedAction)
-    })
-  })
+        filter,
+      };
+      expect(ActionCreators.changeFilter(filter)).toEqual(expectedAction);
+    });
+  });
   describe(ActionCreators.displayMap, () => {
     const value = true;
     it('It should return expected action', () => {
-
       const expectedAction = {
         type: 'DISPLAY_MAP',
-        'showMap': value,
-      }
-      expect(ActionCreators.displayMap(value)).toEqual(expectedAction)
-    })
-  })
-})
+        showMap: value,
+      };
+      expect(ActionCreators.displayMap(value)).toEqual(expectedAction);
+    });
+  });
+});
--- tests/map_reducer_test.js
+++ tests/map_reducer_test.js
@@ -1,18 +1,18 @@
 import MapReducer from '../src/reducers/map_display';
 
 describe(MapReducer, () => {
-    it("Should return a default state of false", () => {
-        expect(MapReducer(undefined, {})).toEqual(false)
-    }) 
+  it('Should return a default state of false', () => {
+    expect(MapReducer(undefined, {})).toEqual(false);
+  });
 
-    it("Should return current state if it's not null", () => {
-        expect(MapReducer("false", {})).toEqual("false")
-    })
+  it("Should return current state if it's not null", () => {
+    expect(MapReducer('false', {})).toEqual('false');
+  });
 
-    it("Should handle DISPLAY_MAP action", () => {
-        expect(MapReducer(undefined, {
-            type: 'DISPLAY_MAP',
-            showMap: true
-        })).toEqual(true)
-    })
-})
+  it('Should handle DISPLAY_MAP action', () => {
+    expect(MapReducer(undefined, {
+      type: 'DISPLAY_MAP',
+      showMap: true,
+    })).toEqual(true);
+  });
+});
